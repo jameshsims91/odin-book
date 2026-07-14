cf096822f02dfdc908b65a95823ff1bf7b018897
jameshsims91 <jameshsims91@gmail.com>
2026-07-14 15:21:38 -0400

    Add root route for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/registrations#new", as: :unauthenticated_root
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
