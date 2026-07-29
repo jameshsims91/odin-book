4a825b24cb3fd3cccbfbda7b61d0ba05af1f679d
jameshsims91 <jameshsims91@gmail.com>
2026-07-29 12:23:08 -0400

    Fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
   def update
     @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
-      format.html { redirect_back_or_to post_path(@post), status: :see_other, notice: "Post updated successfully!" }
+      format { redirect_back_or_to profile_path, status: :see_other, notice: "Post updated successfully!" }
     else
       render :edit, status: :unprocessable_entity
     end
