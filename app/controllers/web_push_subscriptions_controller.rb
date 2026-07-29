7d83ca61df9c8cf06ab0e4bccb5181f44cb7e96a
jameshsims91 <jameshsims91@gmail.com>
2026-07-29 17:18:25 -0400

    Force add missing web push subscription model

--- app/controllers/web_push_subscriptions_controller.rb
+++ app/controllers/web_push_subscriptions_controller.rb
@@ -1,19 +1,4 @@
-class WebPushSubscriptionsController < ApplicationController
-  before_action :authenticate_user!
-
-  def create
-    unless current_user
-      render json: { error: "Unauthoerized" }, status: :unauthorized
-    end
-    subscription = current_user.web_push_subscriptions.find_or_initialize_by(
-      endpoint: params[:endpoint]
-    )
-    subscription.update!(
-      p256dh: params[:subscription][:keys][:p256dh],
-      auth: arams[:subscription][:keys][:auth]
-    )
-    render json: { status: "subscribed" }, status: :ok
-  rescue => e
+class WebPushSubscriptionsController  e
     render json: { error: e.message }, status: :unprocessable_entity
   end
 end
