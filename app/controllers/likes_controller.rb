86f86e805997eee512a82ae8b78d7a9c749f927d
jameshsims91 <jameshsims91@gmail.com>
2026-07-31 01:08:18 -0400

    Implemenet seamless Hotwire Turbo Stream liking interactions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,20 @@ class LikesController < ApplicationController
   before_action :set_post
 
   def create
-    current_user.likes.create(post: @post)
-    redirect_back fallback_location: posts_path
+    current_user.likes.create(post: @post) unless current_user.liked?(@post)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: posts_path }
+    end
   end
 
   def destroy
     like = current_user.likes.find_by(post: @post)
     like&.destroy
-    redirect_back fallback_location: posts_path
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: posts_path }
+    end
   end
 
   private
